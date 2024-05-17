14b29f89ffcddde6cf518277066ed4c0e3567cd7
apexloaded <admin@apexloaded.com>
2024-05-17 15:11:16 +0100

    cors enables

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,11 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:9999',
-      'http://localhost:3002',
-    ],
+    origin: '*',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
-  await app.listen(4100);
+  await app.listen(3000);
 }
 bootstrap();
