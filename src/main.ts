90813c62602243eca88edfafe43bb169dfd6a273
apexloaded <admin@apexloaded.com>
2024-05-31 19:19:25 +0100

    updated cors

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,10 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    //rawBody: true,
     bodyParser: false,
   });
-  // app.useBodyParser('raw');
-  // app.useBodyParser('text');
-  // app.useBodyParser('json', { limit: '50m' });
-  // app.useBodyParser('urlencoded', { limit: '50mb', extented: true });
   app.enableCors({
-    origin: '*',
+    origin: 'https://dexa.ink',
   });
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
